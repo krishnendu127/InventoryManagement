402b78bcb47b4d753826984ad1a232ab12c1749f
krishnendu127 <btech10127.22@gmail.com>
2025-08-24 18:15:16 +0530

    made supplier service

---
+++ Supplier/src/test/java/com/InventoryManagement/Supplier/SupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InventoryManagement.Supplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
